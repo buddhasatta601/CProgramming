458477e0b90b9d83b06720b916a95e55cd4dc190
buddha satta dutta roy <buddhasatta6@gmail.com>
2023-06-20 17:19:51 +0530

    Adding the int char float intDeclearation

---
+++ IntCharFloatDeclearation.c
@@ -0,0 +1,12 @@
+#include<Stdio.h>
+int main(){
+    int a =10;
+    char b ='c';
+    float c =2.2;
+    printf("%d\n",a);
+    printf("%c\n",b);
+    printf("%.1f\n",c);
+   
+    printf("%d\n%c\n%.2f",a,b,c);
+
+}
