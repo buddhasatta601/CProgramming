5f6f21356fbe17583d4c321edf0f3f282fb00b52
buddha satta dutta roy <buddhasatta6@gmail.com>
2023-06-20 17:01:14 +0530

    Adding the  links.md and intDeclearation.c file

---
+++ Links.md
@@ -0,0 +1,2 @@
+100 c programming questions
+https://www.studytonight.com/c/programs/
---
+++ intDeclearation.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int age =20;
+    int age2=22;
+    printf("%d\n",age);
+    printf("%d",age2);
+}
